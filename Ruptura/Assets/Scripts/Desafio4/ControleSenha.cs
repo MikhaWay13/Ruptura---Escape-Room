ff94e078da62425d9580aaf5cba78007068058d6
AmericoMK4 <20241CTB0180007@estudantes.ifpr.edu.br>
2026-09-12 01:46:15 -0300

    OtimizaçãoDeCódigo

--- Ruptura/Assets/Scripts/Desafio4/ControleSenha.cs
+++ Ruptura/Assets/Scripts/Desafio4/ControleSenha.cs
@@ -1,65 +1,28 @@
 using UnityEngine;
 using System.Collections;
+using System.Linq;
 using System;
 
 public class ControleSenha : MonoBehaviour
 {
-    private int[] result, correctCombination;
-
+    private int[] result = new int[] { 0, 0, 0, 0, 0, 0, 0, 0 };
+    private int[] correctCombination = new int[] { 5, 1, 7, 2, 9, 9, 8, 9 };
+    private int indice = 0;
    
     private void Start()
     {
-        result = new int[] {5,5,5,5,5,5,5,5};
-        correctCombination = new int[] { 5, 1, 7, 2, 9, 9, 8, 9};
         RotacaoCofre.Rotated += CheckResults;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     private void CheckResults(string nomeRoda, int numero)
     {
-        switch (nomeRoda)
+         indice = nomeRoda[nomeRoda.Length - 1] - '1';
+
+        result[indice] = numero;
+
+        if (result.SequenceEqual(correctCombination))
         {
-            case "Roda1":
-                result[0] = numero;
-                break;
-
-            case "Roda2":
-                result[1] = numero;
-                break;
-
-            case "Roda3":
-                result[2] = numero;
-                break;
-
-             case "Roda4":
-                result[3] = numero;
-                break;
-
-            case "Roda5":
-                result[4] = numero;
-                break;
-
-            case "Roda6":
-                result[5] = numero;
-                break;
-
-            case "Roda7":
-                result[6] = numero;
-                break;
-
-            case "Roda8":
-                result[7] = numero;
-                break;
-        }
-
-        if(result[0] == correctCombination[0] && result[1] == correctCombination[1] && result[2] == correctCombination[2] && result[3] == correctCombination[3] && result[4] == correctCombination[4]  && result[5] == correctCombination[5] && result[6] == correctCombination[6] && result[7] == correctCombination[7])
-        {
-            print("Abriu");
+            Debug.Log("Abriu");
         }
     }
 }
--- Ruptura/Assets/Scripts/Desafio4/RotacaoCofre.cs
+++ Ruptura/Assets/Scripts/Desafio4/RotacaoCofre.cs
@@ -6,15 +6,12 @@ public class RotacaoCofre : MonoBehaviour
 {
     public static event Action<string, int> Rotated = delegate { };
 
-    private bool coroutineAllowed;
-
-    private int numberShow;
-
-    void Start()
-    {
-        coroutineAllowed = true;
-        numberShow = 5;
-    }
+    [SerializeField] private bool coroutineAllowed = true;
+    [SerializeField] private int controleFor = 11;
+    [SerializeField] private int numberShow = 0;
+    [SerializeField] private float controleCorrou = 0.01f;
+    [SerializeField] private int reiniciaCont = 9;
+    [SerializeField] private Vector3 position = new Vector3(0f, 0f, -3f);
 
     public void Press()
     {
@@ -28,17 +25,17 @@ public class RotacaoCofre : MonoBehaviour
     {
         coroutineAllowed = false;
 
-        for (int i = 0; i <= 11; i++)
+        for (int i = 0; i <= controleFor; i++)
         {
-            transform.Rotate(0f, 0f, -3f);
-            yield return new WaitForSeconds(0.01f);
+            transform.Rotate(position);
+            yield return new WaitForSeconds(controleCorrou);
         }
 
         coroutineAllowed = true;
 
         numberShow += 1;
 
-        if (numberShow > 9)
+        if (numberShow > reiniciaCont)
         {
             numberShow = 0;
         }
